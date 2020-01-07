d12b51ff60467253fff277b41b9bc958ba18a83a
Rosanne <nicolairj@icloud.com>
2020-01-07 15:06:34 +0100

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Rosanne Nicolai on 07/01/2020.
+//  Copyright © 2020 Rosanne Nicolai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
